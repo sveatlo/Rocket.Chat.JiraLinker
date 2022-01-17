0571a25a3f70d1859e1a5c788c3cc65f67a38c2d
Svätopluk Hanzel <svatoplukhanzel@pm.me>
2022-01-17 15:20:38 +0100

    fix: do not search in URLs

--- JiraLinkerApp.ts
+++ JiraLinkerApp.ts
@@ -183,6 +183,10 @@ export class JiraCloudIssueLinkerApp
 
         const matches = message.text
             .replace(/(?:__|[*#])|\[(.*?)\]\(.*?\)/g, "") // ignore issues that are already linked
+            .replace(
+                /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g,
+                ""
+            ) //  ignore whole URLs
             .match(/[A-Z]+-[0-9]+/g); // match possible issue keys
 
         return matches ? matches.concat() : [];
